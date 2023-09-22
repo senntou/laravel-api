04970ab00709f6410cbfa60c5544f4d65b020a71
senntou <wataru_0313@softbank.ne.jp>
2023-09-22 13:40:15 +0900

    modified room model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+    public $timestamps = true;
     protected $fillable = [
         'name',
         'password',
